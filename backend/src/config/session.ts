b90fed6837fc2cd2a3699be42a3a36257c54804e
Gravyon <rdjmartinez95@gmail.com>
2026-01-14 09:21:57 -0300

    update to example variables

--- backend/src/config/session.ts
+++ backend/src/config/session.ts
@@ -1,11 +1,13 @@
 import session from "express-session";
 
 export const sessionConfig: session.SessionOptions = {
+  name: "userSession",
   secret: process.env.SECRET!,
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
